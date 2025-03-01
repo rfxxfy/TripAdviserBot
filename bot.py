272e0d62516190bb336f78fa43d972873f98a12c
Alina Khorst <adkhorst@edu.hse.ru>
2025-03-01 16:23:23 +0300

    Fix core

--- bot.py
+++ bot.py
@@ -1,18 +1,20 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 
 with open("token.txt", "r") as f:
     TOKEN = f.read().strip()
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
-@dp.message_handler(commands=['start'])
 async def welcome(message: types.Message):
-    await message.reply("Hello!")
+    await message.reply("Залупа")
+
+dp.message.register(welcome, Command("start"))
 
 async def main():
-    await dp.start_polling()
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
     asyncio.run(main())
