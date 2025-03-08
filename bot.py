a54948e5286c181367889c2610c53319dfdca0b8
Maria Deminova <mvdeminova@edu.hse.ru>
2025-03-08 13:16:31 +0300

    Update interface

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 with open("token.txt", "r") as f:
     TOKEN = f.read().strip()
@@ -9,7 +10,21 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 async def welcome(message: types.Message):
-    await message.reply("Залупа")
+    welcome_text = """Добро пожаловать в наш бот!
+
+Я могу помочь вам с планированием вашего путешествия.
+Выберите интересующее вас действие:"""
+
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="Построить маршрут")],
+            [KeyboardButton(text="Найти ближайшие обменники валюты с выгодным курсом")],
+            [KeyboardButton(text="Информация про бота")]
+        ],
+        resize_keyboard=True
+    )
+    
+    await message.reply(welcome_text, reply_markup=keyboard)
 
 dp.message.register(welcome, Command("start"))
 
